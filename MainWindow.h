3c526631eabc5670bcf24bfe7f933e36a641d4c7
Juanja1306 <juanjamalovega@gmail.com>
2025-06-05 08:31:00 -0500

    Create MainWindow.h

---
+++ MainWindow.h
@@ -0,0 +1,61 @@
+// MainWindow.h
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QString>
+
+class QPushButton;
+class QLabel;
+class QComboBox;
+class QSlider;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void onLoadImage();
+    void onLoadMask();
+    void onApplyFilter();
+    void onSliderValueChanged(int value);
+    void onMakeVideo();
+    void onOpenVideo();              // Slot para abrir el video
+
+private:
+    // Rutas seleccionadas
+    QString rutaImagenVolumetrica;
+    QString rutaMascaraVolumetrica;
+
+    // Carpeta base para salida (“Output/”)
+    QString carpetaSalidaBase;
+
+    // Widgets de la interfaz
+    QPushButton *btnLoadImage;
+    QPushButton *btnLoadMask;
+    QLabel      *lblImagePath;
+    QLabel      *lblMaskPath;
+
+    QComboBox   *comboFilter;
+    QPushButton *btnApplyFilter;
+
+    // Tres QLabel para mostrar original, máscara y filtrada
+    QLabel      *lblOriginalView;
+    QLabel      *lblMaskView;
+    QLabel      *lblFilteredView;
+    QSlider     *sliderSlice;
+
+    // Botones “Hacer video” y “Abrir video”
+    QPushButton *btnMakeVideo;
+    QPushButton *btnOpenVideo;
+
+    int numSlices;
+
+    void updateSliderRange();
+};
+
+#endif // MAINWINDOW_H
